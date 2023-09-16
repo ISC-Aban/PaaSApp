5db5f920bce5f9e8164f749ab7aab77260ac478b
ISC-Aban <114618340+ISC-Aban@users.noreply.github.com>
2023-09-16 09:21:34 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    html = '''
+    <body style="background-color: black;">
+
+    
+        <h1 style="color: white;">aaaaa finalment</h1>
+        <img src="https://media.discordapp.net/attachments/305058665361309696/1152639559818035230/image.png?width=253&height=236">
+    </body>
+    '''
+    return app.make_response(html)
